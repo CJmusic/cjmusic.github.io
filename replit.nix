9e102d063b323ea1ab6bd26801f1aa87234d3b14
Chris Joseph <christopherzaworski@gmail.com>
2023-03-17 22:29:58 +0000

    Copied environment from --archive, didn't work

--- replit.nix
+++ replit.nix
@@ -1,6 +1,9 @@
 { pkgs }: {
-    deps = [
-        pkgs.ruby_2_6
-		pkgs.rubyPackages_2_7.jekyll
-    ];
+  deps = [
+    pkgs.ruby_2_7
+    pkgs.rubyPackages_2_7.jekyll
+    pkgs.nodejs-16_x
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
 }
