b46b7f3dea84868105d1b4c6882e3d0b1e91433f
Chris Joseph <christopherzaworski@gmail.com>
2023-03-17 23:26:32 +0000

    so close, just the code syntax highlighting is off, may need to recompile

--- replit.nix
+++ replit.nix
@@ -1,10 +1,6 @@
 { pkgs }: {
-  deps = [
-    pkgs.ruby_2_6
-    # pkgs.ruby_3_0
-    # pkgs.rubyPackages_3_1.jekyll
-    # pkgs.nodejs-16_x
-    # pkgs.nodePackages.vscode-langservers-extracted
-    # pkgs.nodePackages.typescript-language-server
+  deps = [pkgs.ruby_3_0
+
+  
   ];
 }
