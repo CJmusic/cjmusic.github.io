b2e723f846be136107c2637a123487ca9585858f
Chris Joseph <christopherzaworski@gmail.com>
2023-03-17 23:01:47 +0000

    CONFIGURED ENVIRONMENT FOR REPLIT FINALLY

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+  deps = [
+    pkgs.ruby_2_6
+    # pkgs.ruby_3_0
+    # pkgs.rubyPackages_3_1.jekyll
+    # pkgs.nodejs-16_x
+    # pkgs.nodePackages.vscode-langservers-extracted
+    # pkgs.nodePackages.typescript-language-server
+  ];
+}
